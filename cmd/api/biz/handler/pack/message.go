51729908508eb78ff8b2aa1f33e64a458d6b731d
lttzz <luckdog.cdr@gmail.com>
2023-02-06 02:13:52 +0800

    feat(api/message): finish api/message

---
+++ cmd/api/biz/handler/pack/message.go
@@ -0,0 +1,32 @@
+package pack
+
+import (
+	"Simple-Douyin/cmd/api/biz/model/api"
+	"Simple-Douyin/kitex_gen/message"
+)
+
+// db.Message -> message.Message
+func Message(msg *message.Message) *api.Message {
+	if msg == nil {
+		return nil
+	}
+
+	return &api.Message{
+		ID:         msg.Id,
+		Content:    msg.Content,
+		CreateTime: msg.CreateTime,
+	}
+}
+
+// []db.Message -> []message.Message
+func Messages(dbmsgs []*message.Message) []*api.Message {
+	msgs := make([]*api.Message, 0)
+
+	for _, m := range dbmsgs {
+		if msg := Message(m); msg != nil {
+			msgs = append(msgs, msg)
+		}
+	}
+
+	return msgs
+}
