2779806f443ffc3f5762190c75113c438e03c9b1
Alexanzhuo <m13505291901@163.com>
2023-02-08 19:23:56 +0800

    fix(favorite):port resused

--- cmd/favorite/main.go
+++ cmd/favorite/main.go
@@ -23,7 +23,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	addr, err := net.ResolveTCPAddr("tcp", "127.0.0.1:8801")
+	addr, err := net.ResolveTCPAddr("tcp", "127.0.0.1:8989")
 	if err != nil {
 		panic(err)
 	}
